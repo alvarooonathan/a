de8c3540505303d371538699a91559e39692f1e5
alvarooonathan <92978481+alvarooonathan@users.noreply.github.com>
2021-12-09 07:00:35 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -23,13 +23,13 @@ return `ℒ⃝👾 *B O T  S T A T I S T I C*
 ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
 
 ⛎ Api : api-alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
-♉ Github : zeeoneofc.github.io
-♊ Instagram : instagram.com/zeeoneofc
-♋ Tiktok : tiktok.com/@zeeoneofc
-♌ Telegram : t.me/zeeoneofc
-♎ Ch Telegram : t.me/zeeone_ofc
-♏ WhatsApp : bit.ly/zeeone_ofc
+♈ YouTube : chrz bot gada yt
+♉ Github : lgi tahap pembuatan
+♊ Instagram : instagram.com/dimdimsptr_
+♋ Tiktok : tiktok.com/@chrzstopher
+♌ Telegram : t.me/ nnti ja
+♎ Ch Telegram : t.me/ belum dibikin males
+♏ WhatsApp : wa.me/6282177500049
 `}
 
 exports.infobot = infobot
