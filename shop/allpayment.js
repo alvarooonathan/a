c245ea2d9afb0416961821f55b64a11b8d263b62
alvarooonathan <92978481+alvarooonathan@users.noreply.github.com>
2021-12-09 07:03:56 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,32 +1,22 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+return `PAYMENT BY NATHAN
 	
 *💰 E-MONEY*
 
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
+1. OVO
+	ㅁ 0895 2017 2279
+2. DANA
+	ㅁ 0895 2017 2279
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama PH**** DI***
 
 *📲 PULSA*
 
-Tidak tersedia
-	
+1. TSEL CARD
+	ㅁ 0821 7750 0049
+2. TRI
+	ㅁ 0895 2017 2279
+
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
 	}
